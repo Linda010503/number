80700e2dcaed27f035012619c990bd7dde27c2b9
Linda010503 <103396630+Linda010503@users.noreply.github.com>
2022-04-19 21:17:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    var x = document.getElementById("myText").value;
+    var y = document.getElementById("Text").value;
+    var z= Number(x) + Number(y);
+    document.getElementById("demo").innerHTML = z ;
+    }
+//Linda(Shen Xiaolin)  Brief description of this JavaScript: Implement the function of outputting the result of adding two numbers.
